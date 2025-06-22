0613b6806687510dbb1ae331e7e3655cba0dad0f
yazhl31 <jjoanyazhini@gmail.com>
2025-06-22 21:04:42 +0530

    Initial commit - HTML CSS project

---
+++ event.js
@@ -0,0 +1,10 @@
+const img = document.getElementById('myImage');
+
+img.onmouseover = function() {
+    console.log('Mouse is over the image.');
+};
+
+img.onmouseout = function() {
+    console.log('Mouse left the image.');    
+    
+};
